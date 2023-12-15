e4b48ad58d855cbbfddf5ace61a832cd479ed071
yoonsla <sy.kim@twolinecode.com>
2023-12-15 18:25:58 +0900

    filter 테스트

---
+++ src/main/java/com/example/filtertest/FilterTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.filtertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/filtertest/FilterTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filtertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
